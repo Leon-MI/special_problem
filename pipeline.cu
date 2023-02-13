3fba1e34f8aa780ba4efec879ae78151fec38cf0
Leon Michalski <leon.michalski@outlook.com>
2023-02-13 12:32:05 +0100

    First bench.

--- pipeline.cu
+++ pipeline.cu
@@ -156,49 +156,28 @@ void save_text(const char * name, const Mat & M) {
     else std::cout << "Unable to open file";
 }
 
-int main()
-{
-    cuda::setDevice(0);
-    // Read and upload img to gpu
-    Mat img_raw = imread("images/frame00000_raw.png", IMREAD_GRAYSCALE);
-    // Mat img_raw = (Mat_<unsigned char>(4,4) << 90, 45, 90, 45, 135, 1, 135, 1, 90, 45, 90, 45, 135, 1, 135, 1);
-    // std::cout << "Raw img rows: " << img_raw.rows << " cols: " << img_raw.cols << " type=" << img_raw.type() << std::endl;
-    
-    GpuMat dev_img;
-    dev_img.upload(img_raw);
 
+void benchmark_indiv(const Mat & img_raw) {
+    const int n = 50;
+    // float t_split [n] = {0.0f};
+    // float t_debayer_mono [n] = {0.0f} ;
+    // float t_stokes [n] = {0.0f};
+    // float t_aolp_dolp [n] = {0.0f};
+    // float t_hsv_rgb [n] = {0.0f};
+    float t_upload = 0.0f;
+    float t_split = 0.0f;
+    float t_debayer_mono = 0.0f;
+    float t_stokes = 0.0f;
+    float t_aolp_dolp = 0.0f;
+    float t_hsv_rgb = 0.0f;
+    float elapsed_time;
     cudaEvent_t start, stop;
-    float elapsedTime;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-
-    // Split
-    cudaEventRecord(start, 0);
     GpuMat dev_bggr0(ROWS2, COLS2, img_raw.type());
     GpuMat dev_bggr45(ROWS2, COLS2, img_raw.type());
     GpuMat dev_bggr90(ROWS2, COLS2, img_raw.type());
     GpuMat dev_bggr135(ROWS2, COLS2, img_raw.type());
-
-    // IMG Size is 2048, 2448, so ideally 5 013 504 threads but can't do that
-    dim3 blocks(64, 77);
-    dim3 threads(32, 32);
-    my_split<<<blocks, threads>>>(dev_img, dev_bggr0, dev_bggr45, dev_bggr90, dev_bggr135);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    printf("Split: %f ms\n", elapsedTime);
-    Mat bggr;
-    dev_bggr0.download(bggr);
-    save_img("bggr0", bggr);
-    dev_bggr45.download(bggr);
-    save_img("bggr45", bggr);
-    dev_bggr90.download(bggr);
-    save_img("bggr90", bggr);
-    dev_bggr135.download(bggr);
-    save_img("bggr135", bggr);
-
-    // Debayer + TGB to Mono
-    cudaEventRecord(start, 0);
     GpuMat dev_rgb0(ROWS2, COLS2, CV_8UC3);
     GpuMat dev_rgb45(ROWS2, COLS2, CV_8UC3);
     GpuMat dev_rgb90(ROWS2, COLS2, CV_8UC3);
@@ -207,102 +186,176 @@ int main()
     GpuMat dev_mono45(ROWS2, COLS2, CV_8UC1);
     GpuMat dev_mono90(ROWS2, COLS2, CV_8UC1);
     GpuMat dev_mono135(ROWS2, COLS2, CV_8UC1);
-    
-    cuda::Stream s0, s45, s90, s135;
-    cuda::cvtColor(dev_bggr0, dev_rgb0, COLOR_BayerBG2BGR, 0, s0);
-    cuda::cvtColor(dev_bggr45, dev_rgb45, COLOR_BayerBG2BGR, 0, s45);
-    cuda::cvtColor(dev_bggr90, dev_rgb90, COLOR_BayerBG2BGR, 0, s90);
-    cuda::cvtColor(dev_bggr135, dev_rgb135, COLOR_BayerBG2BGR, 0, s135);
-
-    cuda::cvtColor(dev_rgb0, dev_mono0, COLOR_BGR2GRAY, 0, s0);
-    cuda::cvtColor(dev_rgb45, dev_mono45, COLOR_BGR2GRAY, 0, s45);
-    cuda::cvtColor(dev_rgb90, dev_mono90, COLOR_BGR2GRAY, 0, s90);
-    cuda::cvtColor(dev_rgb135, dev_mono135, COLOR_BGR2GRAY, 0, s135);
-
-    s0.waitForCompletion();
-    s45.waitForCompletion();
-    s90.waitForCompletion();
-    s135.waitForCompletion();
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    printf("Debayer + RGB to Mono: %f ms\n", elapsedTime);
-    Mat mono;
-    dev_mono0.download(mono);
-    save_img("mono0", mono);
-    dev_mono45.download(mono);
-    save_img("mono45", mono);
-    dev_mono90.download(mono);
-    save_img("mono90", mono);
-    dev_mono135.download(mono);
-    save_img("mono135", mono);
-
-    // Stokes
-    cudaEventRecord(start, 0);
-    blocks.x=32;
-    blocks.y=39;
     GpuMat dev_stokes(ROWS2, COLS2, CV_16SC3);
-    int sdev_stokes = std::ceil((float)dev_stokes.step / sizeof(short3));
-    compute_stokes<<<blocks, threads>>>(dev_mono0, dev_mono45, dev_mono90,
-        dev_mono135, reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes);
-    // CHECK_LAST_CUDA_ERROR();
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    printf("Stokes: %f ms\n", elapsedTime);
-    Mat stokes;
-    dev_stokes.download(stokes);
-    save_img("stokes", stokes);
-    save_text("stokes", stokes);
-
-    // Dolp + Aolp
-    cudaEventRecord(start, 0);
-    cudaStream_t sdolp; 
-    cudaStreamCreate(&sdolp);
-    CHECK_LAST_CUDA_ERROR();
     GpuMat dev_dolp(ROWS2, COLS2, CV_64FC1);
-    compute_dolp<<<blocks, threads, 0, sdolp>>>(reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes, dev_dolp);
-
-
-    // Aolp
-    cudaStream_t saolp; 
-    cudaStreamCreate(&saolp);
     GpuMat dev_aolp(ROWS2, COLS2, CV_64FC1);
-    compute_aolp<<<blocks, threads, 0, saolp>>>(reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes, dev_aolp);
-    CHECK_LAST_CUDA_ERROR();
-
-    cudaStreamDestroy(sdolp);
-    cudaStreamDestroy(saolp);
-
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    printf("dolp + aolp: %f ms\n", elapsedTime);
-
-
-    // False coloring
-    cudaEventRecord(start, 0);
     GpuMat dev_hsv(ROWS2, COLS2, CV_8UC3);
     GpuMat dev_colored(ROWS2, COLS2, CV_8UC3);
-    int sdev_hsv = dev_hsv.step;
-    // int sdev_hsv = std::ceil((float)dev_hsv.step / sizeof(uchar3));
-    false_coloring<<<blocks, threads>>>(dev_aolp, dev_dolp,
-        reinterpret_cast<unsigned char*>(dev_hsv.data), sdev_hsv
-    );
-    // CHECK_LAST_CUDA_ERROR();
-    cuda::cvtColor(dev_hsv, dev_colored, COLOR_HSV2RGB);
-    // CHECK_LAST_CUDA_ERROR();
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    printf("False coloring: %f ms\n", elapsedTime);
-    
-    Mat hsv;
-    dev_hsv.download(hsv);
-    save_img("hsv", hsv);
 
-    Mat colored;
-    dev_colored.download(colored);
-    save_img("colored", colored);
+    for (int i = 0; i < n; i++) {
+        // Upload img
+        cudaEventRecord(start, 0);
+        GpuMat dev_img;
+        dev_img.upload(img_raw);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_upload += elapsed_time;
+
+        // Split
+        dim3 blocks(64, 77);
+        dim3 threads(32, 32);
+        cudaEventRecord(start, 0);
+        my_split<<<blocks, threads>>>(dev_img, dev_bggr0, dev_bggr45, dev_bggr90, dev_bggr135);
+        Mat bggr0, bggr45, bggr90, bggr135;
+        dev_bggr0.download(bggr0);
+        dev_bggr45.download(bggr45);
+        dev_bggr90.download(bggr90);
+        dev_bggr135.download(bggr135);
+
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_split += elapsed_time;
+        bggr0.release();bggr45.release();bggr90.release();bggr135.release();
+
+        // Debayer + RGB To mono
+        blocks.x=32;
+        blocks.y=39;
+        Mat rgb0, rgb45, rgb90, rgb135;
+        Mat mono0, mono45, mono90, mono135;
+
+        cuda::Stream s0, s45, s90, s135;
+        cudaEventRecord(start, 0);
+        cuda::cvtColor(dev_bggr0, dev_rgb0, COLOR_BayerBG2BGR, 0, s0);
+        cuda::cvtColor(dev_bggr45, dev_rgb45, COLOR_BayerBG2BGR, 0, s45);
+        cuda::cvtColor(dev_bggr90, dev_rgb90, COLOR_BayerBG2BGR, 0, s90);
+        cuda::cvtColor(dev_bggr135, dev_rgb135, COLOR_BayerBG2BGR, 0, s135);
+        dev_rgb0.download(rgb0, s0);
+        dev_rgb45.download(rgb45, s45);
+        dev_rgb90.download(rgb90, s90);
+        dev_rgb135.download(rgb135, s135);
+
+        cuda::cvtColor(dev_rgb0, dev_mono0, COLOR_BGR2GRAY, 0, s0);
+        cuda::cvtColor(dev_rgb45, dev_mono45, COLOR_BGR2GRAY, 0, s45);
+        cuda::cvtColor(dev_rgb90, dev_mono90, COLOR_BGR2GRAY, 0, s90);
+        cuda::cvtColor(dev_rgb135, dev_mono135, COLOR_BGR2GRAY, 0, s135);
+        dev_mono0.download(mono0, s0);
+        dev_mono45.download(mono45, s45);
+        dev_mono90.download(mono90, s90);
+        dev_mono135.download(mono135, s135);
+
+        s0.waitForCompletion();
+        s45.waitForCompletion();
+        s90.waitForCompletion();
+        s135.waitForCompletion();
+
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_debayer_mono += elapsed_time;
+
+        rgb0.release(); rgb45.release(); rgb90.release(); rgb135.release();
+        mono0.release(); mono45.release(); mono90.release(); mono135.release();
+
+        // Stokes
+
+        Mat stokes;
+        cudaEventRecord(start, 0);
+        int sdev_stokes = std::ceil((float)dev_stokes.step / sizeof(short3));
+        compute_stokes<<<blocks, threads>>>(dev_mono0, dev_mono45, dev_mono90,
+            dev_mono135, reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes);
+        dev_stokes.download(stokes);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_stokes += elapsed_time;
+        save_img("stokes", stokes);
+        stokes.release();
+
+        // Dolp + Aolp
+        cudaStream_t sdolp; 
+        cudaStreamCreate(&sdolp);
+
+        Mat dolp;
+        cudaStream_t saolp; 
+        cudaStreamCreate(&saolp);
+        Mat aolp;
+    
+        cudaEventRecord(start, 0);
+        compute_dolp<<<blocks, threads, 0, sdolp>>>(reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes, dev_dolp);
+        compute_aolp<<<blocks, threads, 0, saolp>>>(reinterpret_cast<short3*>(dev_stokes.data), sdev_stokes, dev_aolp);
+
+        cudaStreamDestroy(sdolp);
+        cudaStreamDestroy(saolp);
+        dev_dolp.download(dolp);
+        dev_aolp.download(aolp);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_aolp_dolp += elapsed_time;
+        dolp.release(); aolp.release();
+
+        // False coloring
+        Mat colored;
+        cudaEventRecord(start, 0);
+        int sdev_hsv = dev_hsv.step;
+        false_coloring<<<blocks, threads>>>(dev_aolp, dev_dolp,
+            reinterpret_cast<unsigned char*>(dev_hsv.data), sdev_hsv
+        );
+
+        cuda::cvtColor(dev_hsv, dev_colored, COLOR_HSV2RGB);
+        dev_colored.download(colored);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed_time, start, stop);
+        t_hsv_rgb += elapsed_time;
+
+        save_img("colored", colored);
+        colored.release();
+
+        dev_img.release();
+    }
+    dev_bggr0.release();
+    dev_bggr45.release();
+    dev_bggr90.release();
+    dev_bggr135.release();
+    dev_rgb0.release();
+    dev_rgb45.release();
+    dev_rgb90.release();
+    dev_rgb135.release();
+    dev_mono0.release();
+    dev_mono45.release();
+    dev_mono90.release();
+    dev_mono135.release();
+    dev_stokes.release();
+    dev_dolp.release();
+    dev_aolp.release();
+    dev_hsv.release();
+    dev_colored.release();
+    // for (int i = 0; i < n;  i++) {
+    //     std::cout << t_split[n] << " ";
+    // }
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    std::cout << "Turtlebot, mean over " << n << " runs" << std::endl;
+    std::cout << "Host/Device Memory, streams on debayer/mono & aolp/dolp" << std::endl << std::endl;
+    std::cout << "Upload: " << t_upload / n << "ms" << std::endl;
+    std::cout << "Split: " << t_split/n << "ms" << std::endl;
+    std::cout << "Debayer + mono: " << t_debayer_mono / n << "ms" << std::endl;
+    std::cout << "Stokes: " << t_stokes / n << "ms" << std::endl;
+    std::cout << "Aolp + Dolp: " << t_aolp_dolp / n << "ms" << std::endl;
+    std::cout << "False coloring: " << t_hsv_rgb / n << "ms" << std::endl;
+    std::cout << "Total: " << (t_upload + t_split + t_debayer_mono + t_stokes + t_aolp_dolp + t_hsv_rgb) / n << "ms" << std::endl; 
+}
+
+int main()
+{
+    cuda::setDevice(0);
+    // Read and upload img to gpu
+    Mat img_raw = imread("images/frame00000_raw.png", IMREAD_GRAYSCALE);
+
+    benchmark_indiv(img_raw);
+    img_raw.release();
     return 0;
 }
